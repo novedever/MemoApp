14dea90e8cc82444985fd38097744e66a2c8be7b
nove <95565037+novedever@users.noreply.github.com>
2022-01-02 18:02:41 +0900

    Create Loading componet

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -5,11 +5,13 @@ import {
 import { TextInput } from 'react-native-web';
 import Button from '../components/Button';
 import firebase from 'firebase';
+import Loading from '../components/loading';
 
 export default function LoginScreen(props) {
   const { navigation } = props;
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
@@ -18,12 +20,15 @@ export default function LoginScreen(props) {
           index: 0,
           routes: [{ name: 'MemoList' }],
         });
+      } else {
+        setLoading(false);
       }
     });
     return unsubscribe;
   }, []);
 
   function handlePress() {
+    setLoading(true);
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then((userCredential) => {
         const { user } = userCredential;
@@ -35,10 +40,14 @@ export default function LoginScreen(props) {
       })
       .catch((error) => {
         Alert.alert(error.code);
+      })
+      .then(() => {
+        setLoading(false);
       });
   }
   return (
     <View style={styles.container}>
+      <Loading isLoading={isLoading} />
       <View style={styles.inner}>
         <Text style={styles.title}>Log In</Text>
         <TextInput
