10c030bad6d0f1361ebcfd6492916cbd99be6783
nove <95565037+novedever@users.noreply.github.com>
2022-01-09 21:11:55 +0900

    console log delete

--- src/screens/MemoCreateScreen.jsx
+++ src/screens/MemoCreateScreen.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import {
-  View, TextInput, StyleSheet, KeyboardAvoidingView,
+  View, TextInput, StyleSheet, KeyboardAvoidingView, Alert,
 } from 'react-native';
 import firebase from 'firebase';
 import CircleButton from '../components/CircleButton';
+import { translateErrors } from '../utils';
 // import KeyboardSafeView from '../components/KeyboardSafeView';
 
 export default function MemoCreateScreen(props) {
@@ -17,13 +18,13 @@ export default function MemoCreateScreen(props) {
       bodyText,
       updatedAt: new Date(),
     })
-      .then((docRef) => {
-        console.log('Created', docRef.id);
+      .then(() => {
+        navigation.goBack();
       })
       .catch((error) => {
-        console.log('Error', error);
+        const errorMsg = translateErrors(error.code);
+        Alert.alert(errorMsg);
       });
-    navigation.goBack();
   }
 
   return (
--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -33,7 +33,6 @@ export default function MemoListScreen(props) {
         const userMemos = [];
 
         snapshot.forEach((doc) => {
-          console.log(doc.id, doc.data());
           const data = doc.data();
           userMemos.push({
             id: doc.id,
@@ -43,8 +42,7 @@ export default function MemoListScreen(props) {
         });
         setMemos(userMemos);
         setLoading(false);
-      }, (error) => {
-        console.log(error);
+      }, () => {
         setLoading(false);
         Alert.alert('データの読込みに失敗しました。');
       });
